26de6997f57a53d3635d1ee448e48fcde6407412
Adrián Pereira Guerra <adrian.pereira@nomasystems.com>
2021-02-12 15:55:13 +0100

    style: menu-item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, subtitle, imageUrl, size }) => (
-  <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
     <div className="content">
-      <div className="title">{title}</div>
+      <div className="title">{title.toUpperCase()}</div>
       <span className="subtitle">{subtitle}</span>
     </div>
   </div>
