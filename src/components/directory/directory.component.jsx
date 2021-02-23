d40c75cd02a14c3a1fb44deff421cf2a581f7c49
Adrián Pereira Guerra <adrian.pereira@nomasystems.com>
2021-02-23 16:58:34 +0100

    feat: routing to pages detail

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -15,14 +15,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title, imageUrl, size }) => (
-          <MenuItem
-            key={id}
-            title={title}
-            size={size}
-            subtitle="SHOP NOW"
-            imageUrl={imageUrl}
-          />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, subtitle, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
     ></div>
     <div className="content">
       <div className="title">{title.toUpperCase()}</div>
-      <span className="subtitle">{subtitle}</span>
+      <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
