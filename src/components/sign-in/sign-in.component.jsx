d10d2e0555522d1d03e8c8217a603a64a1977400
adrisons <adries.91@gmail.com>
2021-02-26 10:15:41 +0100

    feat: Form input component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -28,22 +29,23 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             required
+            label="Email"
           />
-          <label>Email</label>
-          <input
+
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             required
+            label="Password"
           />
-          <label>Password</label>
 
           <input type="submit" name="Submit Form" />
         </form>
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
-    <SignIn />
+    <SignIn className="sign-in" />
   </div>
 );
 export default SignInAndSignUpPage;
