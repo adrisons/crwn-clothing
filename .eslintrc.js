ac122480a97cc096cc835a48ac5537fc7bd8d823
adrisons <adries.91@gmail.com>
2021-05-24 08:55:11 +0200

    build: configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      arrowFunctions: true,
+      binaryLiterals: true,
+      blockBindings: true,
+      classes: true,
+      defaultParams: true,
+      destructuring: true,
+      forOf: true,
+      generators: true,
+      modules: true,
+      objectLiteralComputedProperties: true,
+      objectLiteralDuplicateProperties: true,
+      objectLiteralShorthandMethods: true,
+      objectLiteralShorthandProperties: true,
+      octalLiterals: true,
+      regexUFlag: true,
+      regexYFlag: true,
+      spread: true,
+      superInFunctions: true,
+      templateStrings: true,
+      unicodeCodePointEscapes: true,
+      globalReturn: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  parser: "babel-eslint",
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      createClass: "createReactClass",
+      pragma: "React",
+      fragment: "Fragment",
+      version: "detect",
+    },
+  },
+};
